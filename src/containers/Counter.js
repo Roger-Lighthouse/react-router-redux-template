695508eae33257b83ecfbb5a803c36db9d3b9404
Stosh Fabricius <stosh.fabricius@gmail.com>
2017-03-27 20:55:19 -0400

    Adding forms

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -6,6 +6,12 @@ import CountHeader from '../components/CountHeader'
 import { fetchCount, increment, decrement } from '../actions/counter'
 
 class CounterContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: ''
+        }
+    }
 
     componentDidMount() {
         // fetch the count
@@ -23,6 +29,26 @@ class CounterContainer extends Component {
         this.props.dispatch(decrement())
     }
 
+    _handleSubmit = (event) => {
+        event.preventDefault()
+        const user = {
+            name: this.state.name
+        }
+        // Here we can dispatch an action
+        // this.props.dispatch(createUser(user))
+        this.setState({ error: null, name: ''})
+
+    }
+
+    _handleChange = (event) => {
+        // We can do validations
+        if (event.target.value.length > 6) {
+            this.setState({ error: 'You input it too long'})
+        } else {
+            this.setState({ error: null, name: event.target.value})
+        }
+    }
+
   render() {
     return (
         <div>
@@ -31,6 +57,14 @@ class CounterContainer extends Component {
             <button onClick={this._increment}>Increment: +</button>
             <button onClick={this._decrement}>Decrement: -</button>
             <button onClick={() => this.props.dispatch(push('/'))}>Click to go to Home</button>
+            { this.state.error ? <p style={{color: 'red'}}>{this.state.error}</p> : null }
+            <form onSubmit={this._handleSubmit}>
+                <label>
+                    Name:
+                    <input name="name" type="text" value={this.state.name} onChange={this._handleChange}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
     );
   }
